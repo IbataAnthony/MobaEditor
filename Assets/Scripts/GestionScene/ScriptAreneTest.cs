fe878b6bd4b91a9f00e1861192405947c86b4334
Hugo Raoult <hugo.raoult@hotmail.fr>
2021-06-02 18:35:03 +0200

    UI v1
    version simple d'une UI (menu principal) avec 4 bouton qui redirige vers les autres scenes

---
+++ Assets/Scripts/GestionScene/ScriptAreneTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptAreneTest : MonoBehaviour
+{
+    public void GoToSceneGame()
+    {
+
+        SceneManager.LoadScene("SceneListCharac");
+    }
+}
+
---
+++ Assets/Scripts/GestionScene/ScriptCreationCharac.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptCreationCharac : MonoBehaviour
+{
+    public void GoToSceneGame()
+    {
+
+        SceneManager.LoadScene("CreationCharac");
+    }
+}
